030eb57dab26865d704384c9c9017b70f95b254a
dalaisubi <suvendu.dalai2@gmail.com>
2018-09-20 04:21:57 +0530

    create a API named, api/product

--- diamond-sweeper/start.js
+++ diamond-sweeper/start.js
@@ -4,10 +4,14 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static('public'))
 
-app.get('/', function (req, res) {
-  res.render("home/index")
+app.get('/api/products', function (req, res) {
+  const product_info = [
+  {"title": "Diamond Sweeper"},
+  {"description": "Bla Bla..."}
+  ];		
+  res.json(product_info);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(5002, function () {
+  console.log('Example app listening on port 5000!')
 });

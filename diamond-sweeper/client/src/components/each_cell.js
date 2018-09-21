c98fb9f3ddaf6f4ea1e2963b3422df9af7120683
dalaisubi <suvendu.dalai2@gmail.com>
2018-09-21 07:43:29 +0530

    functionality for nearest diamond added

--- diamond-sweeper/client/src/components/each_cell.js
+++ diamond-sweeper/client/src/components/each_cell.js
@@ -7,25 +7,86 @@ class EachCell extends Component {
   constructor(props) {
   	super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.nearestElement = this.nearestElement.bind(this);
+    this.digits = this.digits.bind(this);
     this.state = {
       class_name: "unknown",
+      new_cell: this.props.cell_of_diamond
     }
   }	
+
+  digits(n) {
+    return Array.from(String(n), Number)[0];
+  }
+
+  nearestElement(index){
+    let arr_of_diamond = this.state.new_cell.sort() , result = 0;
+    let min_digit = null, max_digit = null;
+
+    if(arr_of_diamond[arr_of_diamond.length - 1] > index){
+
+      for(let i=0; i<arr_of_diamond.length; i++ ) {
+        max_digit = arr_of_diamond[i];
+        if(arr_of_diamond[i-1] != undefined){
+          min_digit = arr_of_diamond[i-1];
+        } 
+        if(max_digit > index){
+          break;
+        }        
+      }
+    }
+    else {
+      min_digit = arr_of_diamond[arr_of_diamond.length - 1];
+    }
+
+    console.log('min_digit :',min_digit, ' index :', index, ' max_digit :' , max_digit , '  : ', this.state.new_cell);
+
+    if(min_digit == undefined && min_digit == null){
+      return max_digit;
+    }
+    else if(max_digit == undefined && max_digit == null){
+      return min_digit;
+    }
+
+    return index - min_digit < max_digit - index? min_digit : max_digit;
+
+  }
+
   handleClick(event){
     var current_class = event.target.attributes[1].value;
-    
+
     if(current_class === "cell unknown" && collected_diamond < 8) {
-      console.log(current_class,'=======');
+
       var index = parseInt(event.target.getAttribute('id'));
+
       if (this.props.cell_of_diamond.indexOf(index) > -1){
         this.setState({
           class_name : "diamond",
         }); 
-        collected_diamond = collected_diamond + 1;    
+        collected_diamond = collected_diamond + 1;
+        var new_arr = this.state.new_cell.indexOf(index);
+        if (new_arr > -1) {
+          this.state.new_cell.splice(new_arr, 1);
+        };    
       }
       else {
+        let nearest_cell = this.nearestElement(index) , cls = null;
+        console.log(nearest_cell, index);
+        if(nearest_cell < index ) {
+          cls = "arrow rotate-left";
+          if(this.digits(nearest_cell) < this.digits(index)){
+            cls = "arrow rotate-up";
+          }
+        }
+        else {
+          cls = "arrow rotate-right";
+          if(this.digits(nearest_cell) > this.digits(index)){
+            cls = "arrow rotate-down";
+          }
+        }
+
         this.setState({
-          class_name : "blank",
+          class_name : cls,
         });
       }
       total_score = total_score - 1;
@@ -33,7 +94,7 @@ class EachCell extends Component {
     
   }	
   render() { 
-    console.log(total_score, collected_diamond);    
+    /*console.log(total_score, collected_diamond);*/    
     return (        
           <td data-tag={this.state.class_name} id={this.props.row + "" + this.props.cell} onClick={this.handleClick} >
             
